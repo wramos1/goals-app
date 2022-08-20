cb21e903317c35ffbcf4d2886f5fea219bc05fd6
Willyam Ramos <ramoswillyam1@gmail.com>
2022-08-19 18:35:40 -0700

    Fixed MongoAtlas issue

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -20,12 +20,12 @@ const protect = asyncHandler(async (req, res, next) => {
         } catch (error) {
             console.log(error)
             res.status(401)
-            throw new Error('Is Not authorized')
+            throw new Error('Not authorized')
         }
     }
     if (!token) {
         res.status(401);
-        throw new Error('Is Not authorized')
+        throw new Error('Not authorized')
     }
 })
 
